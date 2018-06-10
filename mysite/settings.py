634668c0acb1286529662916511846d0f31a3ce3
rplad <39449689+rplad@users.noreply.github.com>
2018-06-10 22:46:40 +0530

    Update settings.py
    Change DEBUG value

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'bs=&_w&dh32!9qh736&3h2e&m^!+%!_3d805z2d_-g5x=db%d3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
