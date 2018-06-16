3d859f0cffe9f8f97f7bd346efddc929cd2974b6
rplad <you@rplad.com>
2018-06-16 13:16:34 +0530

    Cleared for production

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'bs=&_w&dh32!9qh736&3h2e&m^!+%!_3d805z2d_-g5x=db%d3'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
